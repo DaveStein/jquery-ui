2dbc999cf53b4918e4f0e2c1902e7329326a9f47
Mike Sherov <mike.sherov@gmail.com>
2012-12-05 00:07:55 -0500

    enforce the new draggable API in test suite

--- tests/unit/draggable/draggable_common.js
+++ tests/unit/draggable/draggable_common.js
@@ -1,40 +1,47 @@
 TestHelpers.commonWidgetTests( "draggable", {
 	defaults: {
-		appendTo: "parent",
-		axis: false,
-		cancel: "input,textarea,button,select,option",
-		connectToSortable: false,
-		containment: false,
-		cursor: "auto",
-		cursorAt: false,
+		// options
+		appendTo: null,
+		containment: null,
 		disabled: false,
-		grid: false,
-		handle: false,
-		helper: "original",
-		opacity: false,
-		refreshPositions: false,
-		revert: false,
-		revertDuration: 500,
-		scroll: true,
-		scrollSensitivity: 20,
-		scrollSpeed: 20,
-		scope: "default",
-		snap: false,
-		snapMode: "both",
-		snapTolerance: 20,
-		stack: false,
-		zIndex: false,
+		// exclude: "input,textarea,button,select,option",
+		handle: null,
+		helper: false,
 
-		//todo: remove the following option checks when interactions are rewritten:
-		addClasses: true,
-		delay: 0,
-		distance: 1,
-		iframeFix: false,
+		// backCompat options
+		// addClasses: true,
+		// axis: false,
+		// connectToSortable: false,
+		// cursor: "auto",
+		// cursorAt: false,
+		// grid: false,
+		// iframeFix: false,
+		// opacity: false,
+		// refreshPositions: false,
+		// revert: false,
+		// revertDuration: 500,
+		// scroll: true,
+		// scrollSensitivity: 20,
+		// scrollSpeed: 20,
+		// scope: "default",
+		// snap: false,
+		// snapMode: "both",
+		// snapTolerance: 20,
+		// stack: false,
+		// zIndex: false,
+
+		// methods
+		// destroy: null,
+		// disable: null,
+		// enable: null,
+		// option: null,
+		// widget: null,
 
 		// callbacks
-		create: null,
-		drag: null,
-		start: null,
-		stop: null
+		// beforeStart: null,
+		create: null
+		// drag: null,
+		// start: null,
+		// stop: null
 	}
 });
